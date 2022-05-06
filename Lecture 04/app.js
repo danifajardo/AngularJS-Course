b83e46806293e8d85e8a303b8c5bb0e7a0ceb10e
danifajardo <dfajardovargas@gmail.com>
2022-05-06 11:32:07 -0500

    First angular concepts (ng-module, ng-controller)

---
+++ Lecture 04/app.js
@@ -0,0 +1,7 @@
+(function(){
+
+    angular.module('myFirstApp', []) //first parameter: name of the module, second parameter dependencies
+    .controller('myFirstController', function (){ //first parameter: name of controller, second parameter a function
+        //do things
+    }); 
+})();
