70bb033380326a0a4f8bfac54b33a34a6e395803
danifajardo <dfajardovargas@gmail.com>
2022-05-08 18:58:01 -0500

    applying concept od watchers and the ways to declare them

---
+++ Lecture 15/app.js
@@ -0,0 +1,35 @@
+(function(){
+    'use strict';
+
+    angular.module("CounterApp", [])
+    .controller("CounterController", CounterController);
+
+    CounterController.inject = ['$scope'];
+    function CounterController ($scope){
+        $scope.onceCounter = 0;
+        $scope.counter = 0;
+        $scope.name = "";
+
+        $scope.showNumberOfWatchers = function(){
+            console.log("# of watchers: " + $scope.$$watchersCount);
+        };
+
+        $scope.upCounter = function () {
+            $scope.counter++;
+        };
+
+        $scope.countOnce = function(){
+            $scope.onceCounter = 1;
+        };
+
+        // $scope.$watch("onceCounter", function (newValue, oldValue) {
+        //     console.log("onceCounter old value: ", oldValue);
+        //     console.log("onceCounter new value: ", newValue);
+        // });
+
+        // $scope.$watch("counter", function (newValue, oldValue) {
+        //     console.log("counter old value: ", oldValue);
+        //     console.log("counter new value: ", newValue);
+        // });
+    };
+})();
