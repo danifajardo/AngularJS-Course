3d48883fc51774ab1bc4f17b6bca0c5cab3f8ff6
danifajardo <dfajardovargas@gmail.com>
2022-05-16 12:05:08 -0500

    concepts of ng-if, ng-show, ng-hide and apply them

---
+++ lecture 24/app.js
@@ -0,0 +1,79 @@
+(function(){
+    'use strict';
+
+    angular.module("ShoppingListApp", [])
+    .controller("ShoppingListController", ShoppingListController)
+    .provider("ShoppingListService", ShoppingListServiceProvider)
+    .config(Config);
+
+    ShoppingListController.$inject = ["ShoppingListService"];
+    function ShoppingListController(ShoppingListService){
+        var list = this;
+
+        list.items = ShoppingListService.getItems();
+
+        list.itemName = "";
+        list.itemQuantity = "";
+        list.validation = true;
+
+        list.addItem = function(){
+            try {
+                ShoppingListService.addItem(list.itemName,list.itemQuantity);
+            } catch (error) {
+                list.validation = false;
+                list.errorMessage = error.message;
+            }
+        };
+
+        list.removeItem = function(itemId){
+            ShoppingListService.removeItem(itemId);
+        };
+    };
+
+    Config.$inject = ["ShoppingListServiceProvider"];
+    function Config(ShoppingListServiceProvider){
+        ShoppingListServiceProvider.defaults.maxItems = 2; 
+    };
+
+    //Service
+    function ShoppingListService(maxItems){
+        var service = this;
+
+        //list of items
+        var items = [];
+
+        service.addItem = function(name, quantity){
+            if((maxItems === undefined) || ((maxItems !== undefined) && (items.length < maxItems))){
+                var item = {
+                    name: name,
+                    quantity: quantity
+                };
+                items.push(item);
+            }else{
+                throw new Error("Max items (" + maxItems + ") reached."); 
+            }
+        };
+
+        service.removeItem = function(itemId){
+            items.splice(itemId,1);
+        };
+
+        service.getItems = function(){
+            return items;
+        };
+    };
+
+    function ShoppingListServiceProvider(){
+        var provider = this;
+
+        provider.defaults = {
+            maxItems: 10
+        };
+
+        provider.$get = function(){
+            var shoppingList = new ShoppingListService(provider.defaults.maxItems);
+
+            return shoppingList;
+        };
+    };
+})();
